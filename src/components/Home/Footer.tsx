21dfa4f2a6036dd53ceca0f3955b24c838120a75
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-06-24 14:29:17 +0200

    feat: add legal docs to footer

---
+++ src/components/Home/Footer.tsx
@@ -0,0 +1,31 @@
+import React, { ReactElement } from 'react'
+import styles from './Footer.module.css'
+
+export default function Footer(): ReactElement {
+  const year = new Date().getFullYear()
+
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        <div className={styles.copyright}>© {year} All Rights Reserved.</div>
+        <div className={styles.links}>
+          <a
+            href="https://www.delta-dao.com/imprint"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Imprint
+          </a>
+          {' | '}
+          <a
+            href="https://www.delta-dao.com/privacy"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Privacy
+          </a>
+        </div>
+      </div>
+    </footer>
+  )
+}
